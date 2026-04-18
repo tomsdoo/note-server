86362ec0bdabcfb0003a9e68400ef2075ce4a430
tomsdoo <32782986+tomsdoo@users.noreply.github.com>
2026-04-19 07:36:30 +0900

    Tsdown (#50)

--- tsdown.config.ts
+++ tsdown.config.ts
@@ -1,11 +1,14 @@
-import { defineConfig } from "tsup";
+import { defineConfig } from "tsdown";
 
 export default defineConfig({
   entry: ["src/index.ts"],
   clean: true,
   dts: false,
-  splitting: false,
   minify: true,
   format: "esm",
   outDir: "dist",
+  target: false,
+  outputOptions: {
+    codeSplitting: false,
+  },
 });
